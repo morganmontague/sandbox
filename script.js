f81670396707db46e6c5d71323ebb8c724511dcf
C. Morgan Montague <morgan@wmlex.com>
2022-09-16 00:16:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,4 @@
-console.log('Hello')
 
-const select = document.querySelector('select');
-const html = document.querySelector('body');
-document.body.style.padding = '10px';
-
-function update(bgColor, textColor) {
-  html.style.backgroundColor = bgColor;
-  html.style.color = textColor;
-}
-
-select.addEventListener('change', () => select.value === 'black'
-  ? update('black', 'white')
-  : update('white', 'black')
-);
-
-
-// area and volume of a box
-// define area and volume
-// a box is a a container with parallel rectangular or square sides
-// area of a box is 2dw+2dh+2hw, due to there being two parallel sides
-// volume is width*height*depth
-// first create a function for get size
-// function getSize(width, height, depth)
-// set up the two variables area and volume
-// const area = 1
-// const volume = 1
-// then apply the math fomrulas written above
-//  area = (2*depth*width)+(2*depth*height)+(2*width*height)
-//  volume = width*height*depth
-
-function getSize (width, height, depth) {
-    let area = 0
-    let volume = 0
-
-    area = ((2*depth*width)+(2*depth*height)+(2*width*height))
-    volume = width*height*depth
-
-    return [ area, volume]
-}
 
 
 console.log('hello')
@@ -51,4 +12,15 @@ console.log('hello')
 // {
 //    Math.getSum(a,b)
 // }
-
+// we will set compare a and b, whichever is smaller will be x
+// y will be the larger one, if equal set as z
+function getSum(a, b) {
+  let x = Math.min(a, b); 
+  let y = Math.max(a, b);
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+  sum += i
+  }
+  if (x === y) return y;
+  else return sum;
+}
